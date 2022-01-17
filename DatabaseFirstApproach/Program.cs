81cd48cd295df6b5e067b486cd3bc870f8480685
236-Sowmya <s.sowmya9494@gmail.com>
2022-01-17 14:51:35 +0530

    Add project files.

---
+++ DatabaseFirstApproach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseFirstApproach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          using (var context = new CustomerContext())
+            {
+                List<Customers> customers = context.Customers.ToList();
+                foreach(var customer in customers)
+                {
+                    Console.WriteLine(customer.Name);
+                    Console.WriteLine(customer.Location);
+                    Console.WriteLine(customer.Address);
+                    Console.WriteLine(customer.PhoneNumber);
+                }
+            }
+        }
+    }
+}
